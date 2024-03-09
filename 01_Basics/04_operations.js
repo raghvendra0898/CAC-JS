4b54a315bc4a1db51e82a7f19b3c73ddba06c957
raghvendra0898 <117625513+raghvendra0898@users.noreply.github.com>
2024-03-09 09:22:36 +0000

    operations

---
+++ 01_Basics/04_operations.js
@@ -0,0 +1,21 @@
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "Hello"
+let str2 = "Raghvendra"
+let str3 = str1+" "+str2
+console.log(str3)
+
+//confusing results
+console.log("1"+2+2) //if string at first, rest all are treated as string
+console.log(1+2+"2") //is number at first, then all the numbers are treated as numbers
+
+
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+console.log(num1)
+
